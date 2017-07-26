311f3970cabad8ad6eff28b836f34e2f70eeac4a
apple <apple@tang.local>
2017-07-26 17:40:47 +0800

    fast

--- BletcShop/LZDUserInfoVC.m
+++ BletcShop/LZDUserInfoVC.m
@@ -22,12 +22,38 @@
 #import "HobbyChooseVC.h"
 #import "ValuePickerView.h"
 #import "LZDCenterViewController.h"
-@interface LZDUserInfoVC ()<UITableViewDelegate,UITableViewDataSource,NewModelImageViewControllerDelegate,UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIAlertViewDelegate>
+@interface LZDUserInfoVC ()<UITableViewDelegate,UITableViewDataSource,NewModelImageViewControllerDelegate,UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIAlertViewDelegate,UIPickerViewDelegate,UIPickerViewDataSource>
 
 {
     
     NSData *imageData;
     NSData *imageData2;
+    
+    UIPickerView *datePickView;
+    UIToolbar *toolbarCancelDone;
+    
+    NSMutableArray *yearArray;
+    NSArray *monthArray;
+    NSMutableArray *monthMutableArray;
+    NSMutableArray *DaysMutableArray;
+    NSMutableArray *DaysArray;
+    NSString *currentMonthString;
+    NSString *currentDateString;
+    NSString *currentyearString;
+    NSInteger selectedYearRow;
+    NSInteger selectedMonthRow;
+    NSInteger selectedDayRow;
+    
+    BOOL firstTimeLoad;
+    
+    NSInteger m ;
+    int year;
+    int month;
+    int day;
+    
+    NSInteger selectedHourRowOnLine;
+    NSInteger selectedMonthRowOnLine;
+    NSInteger selectedDayRowOnLine;
 }
 @property (nonatomic, strong) ValuePickerView *educationPickerView;
 @property (nonatomic, strong) ValuePickerView *pickerView;//婚姻
@@ -119,7 +145,7 @@
     self.cancleBtn.layer.borderWidth =1;
     self.tishiview.hidden = YES;
     [self.view addSubview:self.tishiview];
-
+    [self _inittableDate];
    
 }
 
@@ -309,7 +335,7 @@
         }
 
     }else if (indexPath.section==2){
-        if (indexPath.row ==0 || indexPath.row ==1/* || indexPath.row ==3|| indexPath.row ==4|| indexPath.row ==5*/) {
+        if (indexPath.row ==0 /*|| indexPath.row ==1 || indexPath.row ==3|| indexPath.row ==4|| indexPath.row ==5*/) {
             UserInfoEditVC *VC = [[UserInfoEditVC alloc]init];
             
             VC.resultBlock=^(NSDictionary*result) {
@@ -325,6 +351,10 @@
             };
             VC.leibie = self.title_A[1][indexPath.row];
             [self.navigationController pushViewController:VC animated:YES];
+        }
+        
+        if (indexPath.row==1) {
+            [self creatPickView];
         }
         
         if (indexPath.row ==2) {
@@ -836,5 +866,262 @@
      }];
     
 }
+#pragma mark 生日
+-(void)_inittableDate
+{
+    
+    m=0;
+    
+    NSDate *date = [NSDate date];
+    
+    // Get Current Year
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy"];
+    
+    currentyearString = [NSString stringWithFormat:@"%@",
+                         [formatter stringFromDate:date]];
+    year =[currentyearString intValue];
+    
+    
+    // Get Current  Month
+    
+    [formatter setDateFormat:@"MM"];
+    
+    currentMonthString = [NSString stringWithFormat:@"%ld",(long)[[formatter stringFromDate:date]integerValue]];
+    month=[currentMonthString intValue];
+    
+    // Get Current  Date
+    
+    [formatter setDateFormat:@"dd"];
+    currentDateString = [NSString stringWithFormat:@"%@",[formatter stringFromDate:date]];
+    
+    day =[currentDateString intValue];
+    
+    
+    yearArray = [[NSMutableArray alloc]init];
+    monthMutableArray = [[NSMutableArray alloc]init];
+    DaysMutableArray= [[NSMutableArray alloc]init];
+    for (int i = year-100; i <= year ; i++)
+    {
+        [yearArray addObject:[NSString stringWithFormat:@"%d",i]];
+        
+    }
+    
+    monthArray = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12"];
+    
+    for (int i=1; i<month+1; i++) {
+        [monthMutableArray addObject:[NSString stringWithFormat:@"%d",i]];
+    }
+    DaysArray = [[NSMutableArray alloc]init];
+    
+    for (int i = 1; i <= 31; i++)
+    {
+        [DaysArray addObject:[NSString stringWithFormat:@"%d",i]];
+        
+    }
+    for (int i = 1; i <day+1; i++)
+    {
+        [DaysMutableArray addObject:[NSString stringWithFormat:@"%d",i]];
+        
+    }
+    
+}
+-(void)creatPickView{
+    
+    
+    datePickView = [[UIPickerView alloc]initWithFrame:CGRectMake(0, SCREENHEIGHT-160-64, SCREENWIDTH, 160)] ;
+    datePickView.backgroundColor = [UIColor whiteColor];
+    datePickView.delegate = self;
+    datePickView.dataSource = self;
+    toolbarCancelDone = [[UIToolbar alloc]initWithFrame:CGRectMake(0, SCREENHEIGHT-200-64, SCREENWIDTH, 40)];
+    [self.view addSubview: datePickView];
+    UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    okBtn.frame = CGRectMake(SCREENWIDTH-60, 0, 60, 40);
+    [okBtn setTitle:@"确定" forState:UIControlStateNormal];
+    //[okBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [okBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    okBtn.titleLabel.font = [UIFont systemFontOfSize:15];
+    [okBtn addTarget:self action:@selector(actionDone) forControlEvents:UIControlEventTouchUpInside];
+    [toolbarCancelDone addSubview:okBtn];
+    UIButton *cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    cancelBtn.frame = CGRectMake(0, 0, 60, 40);
+    [cancelBtn setTitle:@"取消" forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    [cancelBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    cancelBtn.titleLabel.font = [UIFont systemFontOfSize:15];
+    [cancelBtn addTarget:self action:@selector(actionCancel) forControlEvents:UIControlEventTouchUpInside];
+    [toolbarCancelDone addSubview:cancelBtn];
+    
+    [self.view addSubview:toolbarCancelDone];
+    
+    [datePickView selectRow:[yearArray indexOfObject:currentyearString] inComponent:0 animated:NO];
+    
+    [datePickView selectRow:[monthArray indexOfObject:currentMonthString] inComponent:1 animated:YES];
+    
+    
+}
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    UILabel *pickerLabel = (UILabel *)view;
+    
+    if (pickerLabel == nil) {
+        CGRect frame = CGRectMake(0.0, 0.0, 50, 60);
+        pickerLabel = [[UILabel alloc] initWithFrame:frame];
+        [pickerLabel setTextAlignment:NSTextAlignmentCenter];
+        [pickerLabel setBackgroundColor:[UIColor clearColor]];
+        [pickerLabel setFont:[UIFont systemFontOfSize:15.0f]];
+    }
+    if (component == 0)
+    {
+        pickerLabel.text =  [yearArray objectAtIndex:row]; // Year
+    }
+    else if (component == 1)
+    {
+        pickerLabel.text =  [monthArray objectAtIndex:row];  // Month
+    }
+    else if (component == 2)
+    {
+        pickerLabel.text =  [DaysArray objectAtIndex:row]; // Date
+        
+    }
+    
+    
+    return pickerLabel;
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 3;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    {
+        if (component == 0)
+        {
+            return [yearArray count];
+            
+        }
+        else if (component == 1)
+        {
+            NSInteger selectRow =  [pickerView selectedRowInComponent:0];
+            int n;
+            n= year-1970;
+            if (selectRow==n) {
+                return [monthMutableArray count];
+            }else
+            {
+                return [monthArray count];
+                
+            }
+        }
+        else
+        {
+            NSInteger selectRow1 =  [pickerView selectedRowInComponent:0];
+            int n;
+            n= year-1970;
+            NSInteger selectRow =  [pickerView selectedRowInComponent:1];
+            
+            if (selectRow==month-1 &selectRow1==n) {
+                
+                return day;
+                
+            }else{
+                
+                if (selectedMonthRow == 0 || selectedMonthRow == 2 || selectedMonthRow == 4 || selectedMonthRow == 6 || selectedMonthRow == 7 || selectedMonthRow == 9 || selectedMonthRow == 11)
+                {
+                    return 31;
+                }
+                else if (selectedMonthRow == 1)
+                {
+                    int yearint = [[yearArray objectAtIndex:selectedYearRow]intValue ];
+                    
+                    if(((yearint %4==0)&&(yearint %100!=0))||(yearint %400==0)){
+                        return 29;
+                    }
+                    else
+                    {
+                        return 28; // or return 29
+                    }
+                    
+                }
+                else
+                {
+                    return 30;
+                }
+            }
+        }
+    }
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    m=row;
+    
+    
+    
+    if (component == 0)
+    {
+        selectedYearRow = row;
+        [datePickView reloadAllComponents];
+    }
+    else if (component == 1)
+    {
+        selectedMonthRow = row;
+        [datePickView reloadAllComponents];
+    }
+    else if (component == 2)
+    {
+        selectedDayRow = row;
+        
+        [datePickView reloadAllComponents];
+        
+    }
+    
+    
+}
+
+- (void)actionCancel
+{
+    
+    [UIView animateWithDuration:0.5
+                          delay:0.1
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         
+                         datePickView.hidden = YES;
+                         toolbarCancelDone.hidden = YES;
+                         
+                         
+                     }
+                     completion:^(BOOL finished){
+                         
+                         
+                     }];
+    
+    
+}
+- (void)actionDone
+{
+    
+    NSString *birthday  = [NSString stringWithFormat:@"%@-%@-%@",[yearArray objectAtIndex:[datePickView selectedRowInComponent:0]],[monthArray objectAtIndex:[datePickView selectedRowInComponent:1]],[DaysArray objectAtIndex:[datePickView selectedRowInComponent:2]]];
+    [self postRevise:birthday type:@"age"];
+    
+    [UIView animateWithDuration:0.5
+                          delay:0.1
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         
+                         datePickView.hidden = YES;
+                         toolbarCancelDone.hidden = YES;
+                         
+                         
+                     }
+                     completion:^(BOOL finished){
+                         
+                         
+                     }];
+    
+}
+
+
 
 @end
