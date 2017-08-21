3c132fd28c42658285d94bfb8e38cb498de543d0
apple <apple@tang.local>
2017-08-21 10:24:58 +0800

    发生的

---
+++ BletcShop/NewAddCardDetailVC.m
@@ -0,0 +1,38 @@
+//
+//  NewAddCardDetailVC.m
+//  BletcShop
+//
+//  Created by apple on 2017/8/18.
+//  Copyright © 2017年 bletc. All rights reserved.
+//
+
+#import "NewAddCardDetailVC.h"
+
+@interface NewAddCardDetailVC ()
+@property (weak, nonatomic) IBOutlet UIView *view1;
+
+@end
+
+@implementation NewAddCardDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title=@"逗你玩";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
